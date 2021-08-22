a3ba1e5fa449c624d3a545554f3a2c3bdf876c9d
AkbarAzad <akbarazad93@gmail.com>
2021-08-22 14:54:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -269,7 +269,7 @@ def updateCharts(company, start_date, end_date):
     maChartFigure = px.line(x = filteredData['date'], y = filteredData['close'], name = 'Original', title = ' Original Stock Price vs. 21-days Moving Average change')
     maChartFigure.add_scatter(x = filteredData['date'], y = filteredData['ma'], name = '21-days moving average')
     
-    ridgeChartFigure = px.line(x = filteredData['date'], y = filteredData['close'], name = 'Original', title = f"Original Scaled Close Price vs Prediction with Accuracy {filteredData['accuracy'][0]}")
+    ridgeChartFigure = px.line(x = filteredData['date'], y = filteredData['close'], name = 'Original', title = "Original Scaled Close Price vs Prediction")
     ridgeChartFigure.add_scatter(x = filteredData['date'], y = filteredData['ma'], name = 'Predictions')
 
     return closeChartFigure, normalisedChartFigure, histogramChartFigure, returnsChartFigure, maChartFigure, ridgeChartFigure
