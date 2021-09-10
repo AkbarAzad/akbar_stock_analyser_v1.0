0f871c7b4a87710fade838ff481300d8efbad838
AkbarAzad <akbarazad93@gmail.com>
2021-09-10 15:04:42 +0800

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -3,7 +3,7 @@ from app import calculateSum
 
 class myTest(unittest.TestCase):
 
-    def testFunction():
+    def testFunction(self):
         self.assertEqual(calculateSum(1,1), 2)
         self.assertEqual(calculateSum(1,-1), 0)
         
