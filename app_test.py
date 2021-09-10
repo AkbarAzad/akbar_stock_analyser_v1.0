603e4ee524bd0c6aa328eadf1bb64676f85c9211
AkbarAzad <akbarazad93@gmail.com>
2021-09-10 14:22:41 +0800

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -4,7 +4,7 @@ from app import calculateSum
 class myTest(unittest.TestCase):
 
     def testFunction():
-        self.assertEqual(calculateSum(1,1) 2)
+        self.assertEqual(calculateSum(1,1), 2)
         self.assertEqual(calculateSum(1,-1), 0)
         
 if __name__ == '__main__':
